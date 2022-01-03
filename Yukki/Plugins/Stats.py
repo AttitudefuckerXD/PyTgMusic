b3c50caa780bb3f0f3590b211c72ccd801bcf30c
AttitudeKing-vj <95028968+AttitudeKing-vj@users.noreply.github.com>
2022-01-03 16:54:43 +0530

    Update Stats.py

--- Yukki/Plugins/Stats.py
+++ Yukki/Plugins/Stats.py
@@ -57,7 +57,7 @@ async def gstats(_, message):
         pass
     uptime = await bot_sys_stats()
     response = await message.reply_photo(
-        photo="Utils/Query.jpg", caption="Getting Stats!"
+        photo="https://telegra.ph/file/8a4097ee3cec33b1814e1.jpg", caption="Getting Stats!"
     )
     end = datetime.now()
     resp = (end - start).microseconds / 1000
